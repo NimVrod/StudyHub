e13b50661a96c2aadb40e94b55dd832aa2b811ec
NimVrod <hdec0759@gmail.com>
2022-02-23 18:11:42 +0100

    Push

--- StudyHub/quizes/admin.py
+++ StudyHub/quizes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import quiz, pytanie, odpowiedz
 
 # Register your models here.
+admin.site.register(quiz)
--- StudyHub/quizes/models.py
+++ StudyHub/quizes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class quiz(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    img = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+class pytanie(models.Model):
+    name = models.CharField(max_length=200)
+    q = models.ForeignKey(quiz, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class odpowiedz(models.Model):
+    name = models.CharField(max_length=200)
+    p = models.ForeignKey(pytanie, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return self.name
