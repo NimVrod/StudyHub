82729151b4a604adb5a368833480d37049d3630a
NimVrod <84095520+NimVrod@users.noreply.github.com>
2022-06-14 21:54:16 +0200

    Update settings.py

--- StudyHub/StudyHub/settings.py
+++ StudyHub/StudyHub/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%!dn5m%ynr8@@291g0#5$(b4_wy#wqxeb-0gz=h007p0in8&*3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
